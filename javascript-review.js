9533a3b5b1be5965a4e1a1adefa7032571592ac5
Harrison Malone <harrison.malone2@gmail.com>
2019-09-18 09:11:26 +1000

    initial commit

---
+++ javascript-review.js
@@ -0,0 +1,57 @@
+// You may NOT use Google. These questions are to test how you are progressing through the material, and to give you some feedback about where you might improve. Get through as many of these as you can. If you don’t get through all of them it’s ok - the test is designed to be difficult. If you can’t remember a bit of code, write the pseudo-code that represents how you would write your JS code.
+
+// 1.
+// Create an object that has four properties. One property should be set so that the value is a number, another property set to a string, the third to an array, and the fourth to a function. This function should simply console.log the value of the first property.
+
+// 2.
+// Write a factory function (to create objects) that takes one argument. It should use that argument to set the value of the first key of the object. The second key should be given the value ‘red’. 
+
+// 3.
+// Can you access variables defined outside of functions within the scope of functions in JS? Show a simple example of this being used? (Define a constant in global scope and use that in a very simple function.)
+
+// 4.
+// What two ways can be used to access the properties of objects in JS? Give examples of both (although assume the objects have been defined).
+
+// 5.
+// What is JSON? Be clear but concise.
+
+// 6.
+// What is a callback?
+
+// 7.
+// Define a function with that takes two number arguments and a callback. The function will add the two numbers and pass them to the callback as an argument. Now call this function and in the callback simply console.log out the argument that has been passed through.
+
+// 8.
+// Why do we use promises?
+
+// 9.
+// What are the two methods at our disposal if we have hold of a promise? What are the names of the functions that are related to these functions (that are called when the promise is fulfilled or fails). 
+// 10.
+// What is the DOM?
+
+// 11.
+// Create an object that has three properties. One property is called ‘count’ and will be set to a number, the second will be set to an array, and the third to a function. This function should simply console.log each of the values of the second property on a new line.
+
+// 12.
+// What is NPM? In your brief explanation refer to package.json.
+
+// 13.
+// What are Event Listeners? Gives some examples in your response.
+
+// 14.
+// Define a function called numMult that takes two number arguments and a callback. The function will multiply the two numbers and pass the result to the callback as an argument. Now call numMult and in the callback simply console.log out the argument that has been passed through.
+
+// 15.
+// Define a function called addNum with that takes two number arguments. In this function simply return the addition of these numbers. Write a second function called numsPlusFunct that takes three arguments, two numbers and a function. Inside numsPlusFunct call the function that is passed as an argument, and pass the two number arguments to this function. numsPlusFunct will return an object where the first property has the value returned from that function call, and the second property is a string. You have now made two functions. Call the numsPlusFunct and pass addNum as the appropriate argument. 
+
+// 16.
+// What is Express? What does it help us to do? Often we use ‘req’ and ‘res’ in our Express/Node code. Very briefly outline what these are.
+
+// 17.
+// What is the difference between synchronous and asynchronous code? Name some ways that JS deals with the issue of asynchronous code. 
+
+// 18.
+// What is fetch and how does it relate to AJAX? Give an example of how you would use it. What does fetch return? Give a very basic example of fetch.
+
+// 19.
+// A JS object looks like this: const southernField = { location: “upper”, crop: “sorghum”, watered: false }. Use destructuring to store the value of watered in a variable.
