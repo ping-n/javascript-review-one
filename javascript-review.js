c0480d7bf535d3a01134bd58173f5d61bfccf9d0
Harrison Malone <harrison.malone2@gmail.com>
2019-09-19 13:36:51 +1000

    slight edit

--- javascript-review.js
+++ javascript-review.js
@@ -25,7 +25,8 @@
 // Why do we use promises?
 
 // 9.
-// What are the two methods at our disposal if we have hold of a promise? What are the names of the functions that are related to these functions (that are called when the promise is fulfilled or fails). 
+// What are the two methods at our disposal if we have hold of a promise? What are the names of the functions that are related to these functions (that are called when the promise is fulfilled or fails).
+
 // 10.
 // What is the DOM?
 
@@ -42,7 +43,15 @@
 // Define a function called numMult that takes two number arguments and a callback. The function will multiply the two numbers and pass the result to the callback as an argument. Now call numMult and in the callback simply console.log out the argument that has been passed through.
 
 // 15.
-// Define a function called addNum with that takes two number arguments. In this function simply return the addition of these numbers. Write a second function called numsPlusFunct that takes three arguments, two numbers and a function. Inside numsPlusFunct call the function that is passed as an argument, and pass the two number arguments to this function. numsPlusFunct will return an object where the first property has the value returned from that function call, and the second property is a string. You have now made two functions. Call the numsPlusFunct and pass addNum as the appropriate argument. 
+
+// a
+// Define a function called addNum with that takes two number arguments. In this function simply return the addition of these numbers. 
+
+// b
+// Write a second function called numsPlusFunct that takes three arguments, two numbers and a function. Inside numsPlusFunct call the function that is passed as an argument, and pass the two number arguments to this function. numsPlusFunct will return an object where the first property has the value returned from that function call, and the second property is a string. 
+
+// c
+// You have now made two functions. Call the numsPlusFunct and pass addNum as the appropriate argument. 
 
 // 16.
 // What is Express? What does it help us to do?
